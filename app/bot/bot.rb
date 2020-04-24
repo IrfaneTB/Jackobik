b10bf2500ded2584528f05e1e94bd3d65dfe5128
IrfaneTB <youniverse-up@outlook.fr>
2020-04-24 13:38:05 +0400

    code ping

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -8,6 +8,13 @@ bot.message(with_text: "!Hey Bot!") do |event|
     event.respond "Hi, #{event.user.name}!"
   end
 
+  Bot.message(content: '!ping') do |event|
+    event.respond'pong'
+  end
+
+
+
+
   bot.run
 
 
