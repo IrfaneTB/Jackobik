cae6f68d4753e272da8b591dd724fe3e4f70c074
IrfaneTB <youniverse-up@outlook.fr>
2020-04-24 12:42:09 +0400

    frist commit

---
+++ app/bot/bot.rb
@@ -0,0 +1,14 @@
+require 'discordrb'
+
+bot = Discordrb::Bot.new token: 'NjQ2NjE4MTc5OTQyMDg4NzA1.XqKW4g.fg5xriA8NXYoxWsvcgPYhu5d6ac'
+
+bot = Discordrb::Commands::CommandBot.new token: 'NjQ2NjE4MTc5OTQyMDg4NzA1.XqKW4g.fg5xriA8NXYoxWsvcgPYhu5d6ac', client_id: 646618179942088705, prefix: '!'
+
+bot.message(with_text: "Hey Bot!") do |event|
+    event.respond "Hi, #{event.user.name}!"
+  end
+
+  bot.run
+
+
+
