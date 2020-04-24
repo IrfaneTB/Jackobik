813561e8d04af8523d5c5a69eaec14f316aa87a0
IrfaneTB <youniverse-up@outlook.fr>
2020-04-24 13:24:12 +0400

    env tokens changed

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,8 +1,8 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: Tokens
+bot = Discordrb::Bot.new token: ENV["Tokens"]
 
-bot = Discordrb::Commands::CommandBot.new token: Tokens, client_id: 646618179942088705, prefix: '!'
+bot = Discordrb::Commands::CommandBot.new token: ENV["Tokens"], client_id: 646618179942088705, prefix: '!'
 
 bot.message(with_text: "!Hey Bot!") do |event|
     event.respond "Hi, #{event.user.name}!"
