665bb467d5b285b9fdc2e599d20e9f26e9c0193f
IrfaneTB <youniverse-up@outlook.fr>
2020-04-24 13:17:56 +0400

    frist commit 0.2

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,8 +1,8 @@
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: 'NjQ2NjE4MTc5OTQyMDg4NzA1.XqKW4g.fg5xriA8NXYoxWsvcgPYhu5d6ac'
+bot = Discordrb::Bot.new token: Tokens
 
-bot = Discordrb::Commands::CommandBot.new token: 'NjQ2NjE4MTc5OTQyMDg4NzA1.XqKW4g.fg5xriA8NXYoxWsvcgPYhu5d6ac', client_id: 646618179942088705, prefix: '!'
+bot = Discordrb::Commands::CommandBot.new token: Tokens, client_id: 646618179942088705, prefix: '!'
 
 bot.message(with_text: "!Hey Bot!") do |event|
     event.respond "Hi, #{event.user.name}!"
